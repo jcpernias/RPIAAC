079d3be5211a2364c4d6651432881d86c7b6fc9d
José C. Pernías <jcpernias@gmail.com>
2016-07-25 20:45:21 +0200

    Basic documentation of datasets

--- R/data.R
+++ R/data.R
@@ -1,1 +1,258 @@
 # This file documents datasets
+
+#' Austria
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"aut"
+
+#' Belgium (Flanders)
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"bel"
+
+
+#' Canada
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"can"
+
+#' Chile
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"chl"
+
+#' Cyprus
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"cyp"
+
+#' Czech Republic
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"cze"
+
+#' Germany
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"deu"
+
+#' Denmark
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"dnk"
+
+#' Spain
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"esp"
+
+#' Estonia
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"est"
+
+#' Finland
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"fin"
+
+#' France
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"fra"
+
+#' United Kingdom (England and Northern Ireland)
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"gbr"
+
+#' Greece
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"grc"
+
+#' Indonesia
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"idn"
+
+#' Ireland
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"irl"
+
+#' Israel
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"isr"
+
+#' Italy
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"ita"
+
+#' Japan
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"jpn"
+
+#' South Korea
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"kor"
+
+#' Lithuania
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"ltu"
+
+#' Netherlands
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"nld"
+
+#' Norway
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"nor"
+
+#' New Zealand
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"nzl"
+
+#' Poland
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"pol"
+
+#' Russian Federation
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"rus"
+
+#' Singapore
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"sgp"
+
+#' Slovak Republic
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"svk"
+
+#' Slovenia
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"svn"
+
+#' Sweden
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"swe"
+
+#' Turkey
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"tur"
+
+#' United States
+#'
+#' Datasets from public-use files of the OECD Programme for the International
+#' Assesment of Adult competencies.
+#'
+#' @source \url{http://www.oecd.org/skills/piaac/}
+"usa"
