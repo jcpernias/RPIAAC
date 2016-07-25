39e8f9ba166d24a4432bc8adf3c0e06e9d616c01
José C. Pernías <jcpernias@gmail.com>
2016-07-25 22:19:43 +0200

    Package documentation

--- R/RPIAAC.R
+++ R/RPIAAC.R
@@ -1,2 +1,10 @@
 # This file documents the package
 
+#' RPIAAC: datasets from the Survey of Adult Skills (PIAAC)
+#'
+#' This packages provides the dta contained in the public-use files of the first
+#' and second round of the PIAAC.
+#'
+#' @docType package
+#' @name RPIAAC
+NULL
